d2cddf1eb4caa2353ac16b3aa1d29671062fb12e
Joseph Riley <sc21jr@leeds.ac.uk>
2022-07-24 12:45:16 +0100

    Forgot to save when I was text editing

---
+++ assignment_1/pgma2b.h
@@ -0,0 +1,8 @@
+#ifndef __PGM_A2B__
+#define __PGM_A2B__
+#include "pgmImage.h"
+#include "pgmErrors.h"
+#include "pgmRead.h"
+#include "pgmWrite.h"
+int main(int argc, char** argv);
+#endif //__PGM_A2B__
